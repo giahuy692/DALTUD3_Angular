a05fa96b0f72a786eb5a600695ab3ef0e8299369
ntantai2526 <ntantai2526@gmail.com>
2023-05-30 13:58:35 +0700

    Tạo commit giải quyết xung đột

--- src/app/p-shop/share/components/product/product.component.ts
+++ src/app/p-shop/share/components/product/product.component.ts
@@ -6,27 +6,13 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent {
-<<<<<<< HEAD
-=======
-  private _review: number = 0;
->>>>>>> main
   @Input() category: string = '';
   @Input() imgProduct: string = '';
   @Input() titleProduct: string = '';
   @Input() price: number = 0;
   @Input() discount: number = 0;
-<<<<<<< HEAD
   @Input() review: number[] = Array.from({ length: 5 });
 
-=======
-  @Input() set review(value: number) {
-    this._review = Math.round(value);
-  }
-  get review(): number {
-    return this._review;
-  }
-  @Input() countRate: number = 0;
->>>>>>> main
   constructor() {}
 
   ngOnInit() {}
