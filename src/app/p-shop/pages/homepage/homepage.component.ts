a1f49f6b3fbe63b6936a56013387f794c3e2f56e
ntantai2526 <ntantai2526@gmail.com>
2023-06-04 13:04:33 +0700

    Hompage - Ui - update: hoàn thành hiển thị sản phẩm trên trên trang hompage câph nhật lại scss và component

--- src/app/p-shop/pages/homepage/homepage.component.ts
+++ src/app/p-shop/pages/homepage/homepage.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit, ViewChild, OnDestroy, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  OnDestroy,
+  AfterViewInit,
+} from '@angular/core';
 import { ShopApiService } from '../../share/services/shop-api.service';
+import { DTOProduct } from '../../share/dtos/DTOProduct';
+import { NotificationService } from '@progress/kendo-angular-notification';
 
 export interface Item {
   title?: string;
@@ -11,9 +19,14 @@ export interface Item {
   styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent implements OnInit {
-  apiService: any;
-  data: any;
-  constructor() {}
+  data: DTOProduct[];
+  productSingle: any;
+  listProductLimit: any;
+
+  constructor(
+    private apiService: ShopApiService,
+    private notificationService: NotificationService
+  ) {}
 
   ngOnInit(): void {
     this.getData();
@@ -22,53 +35,65 @@ export class HomepageComponent implements OnInit {
   }
 
   getData() {
-    this.apiService.getListProduct().subscribe((v: any) => {
-      this.data = v;
-      console.log(v);
-    });
+    this.apiService.getListProduct().subscribe(
+      (v: any) => {
+        this.data = v;
+        this.notificationService.show({
+          content: 'Get list product success',
+          hideAfter: 600,
+          position: { horizontal: 'left', vertical: 'bottom' },
+          animation: { type: 'fade', duration: 400 },
+          type: { style: 'success', icon: true },
+        });
+      },
+      (error) => {
+        this.notificationService.show({
+          content: error,
+          animation: { type: 'slide', duration: 400 },
+          position: { horizontal: 'center', vertical: 'bottom' },
+          type: { style: 'error', icon: true },
+        });
+      }
+    );
   }
 
   getProductSingle() {
     this.apiService.getProduct(1).subscribe((v: any) => {
-      this.getProductSingle = v;
+      this.productSingle = v;
     });
   }
 
   getListProductLimit() {
     this.apiService.getListProductLimit(5).subscribe((v: any) => {
-      this.getListProductLimit = v;
+      this.listProductLimit = v;
     });
   }
 
   // =====================================================================
-@ViewChild("sv") private scrollView: any;
+  @ViewChild('sv') private scrollView: any;
 
-  myData:any =  [
-    { title: "Flower", url: "https://bit.ly/2cJjYuB" },
-    { title: "Mountain", url: "https://bit.ly/2cTBNaL" },
-    { title: "Sky", url: "https://bit.ly/2cJl3Cx" },
+  myData: any = [
+    { title: 'Flower', url: 'https://bit.ly/2cJjYuB' },
+    { title: 'Mountain', url: 'https://bit.ly/2cTBNaL' },
+    { title: 'Sky', url: 'https://bit.ly/2cJl3Cx' },
   ];
   public paused = false;
   public items: Item[] = this.myData;
-  public width = "100%";
-  public height = "500px";
+  public width = '100%';
+  public height = '500px';
   private interval: any;
 
   public ngAfterViewInit(): void {
     this.interval = setInterval(() => {
-      
-        this.scrollView.next();
-      
+      this.scrollView.next();
     }, 3000);
   }
 
   public ngOnDestroy(): void {
     clearInterval(this.interval);
   }
-//=========================================================================
+  //=========================================================================
   public onButtonClick(): void {
-    console.log("click");
+    console.log('click');
   }
-
-
 }
