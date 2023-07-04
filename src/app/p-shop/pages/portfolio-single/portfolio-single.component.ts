4ea480fccd866be74d2a327e7d1781c20330397d
LAPTOP-QQSHN285\phung <phungquantruong@gmail.com>
2023-07-04 13:09:12 +0700

    Portfolio-single-html/css-Edit: thay img banner, thay img content, chỉnh font size title banner, font size content, subtitle, button size. Fix lỗi layout bị tràn ra khỏi banner khi zoom.

--- src/app/p-shop/pages/portfolio-single/portfolio-single.component.ts
+++ src/app/p-shop/pages/portfolio-single/portfolio-single.component.ts
@@ -7,15 +7,5 @@ import { TileLayoutGap } from '@progress/kendo-angular-layout';
   styleUrls: ['./portfolio-single.component.scss'],
 })
 export class PortfolioSingleComponent {
-  public gap: TileLayoutGap = {
-    rows: 25,
-    columns: 40,
+  
   };
-
-  public firstTileContent = `t is a long established fact that a reader will be distracted by the readable content of a page when looking a layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
-  Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and auncover many web sites still in their infancy. Various versions have evolved over the years`;
-  public secondTileContent = `It is a long established fact that a reader will be distracted by the readable content of a page when looking a layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
-  Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and auncover many web sites still in their infancy. Various versions have evolved over the years`;
-  public thirdTileContent = `t is a long established fact that a reader will be distracted by the readable content of a page when looking a layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. 
-  Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and auncover many web sites still in their infancy. Various versions have evolved over the years`;
-}
