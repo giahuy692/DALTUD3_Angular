2f889632a67def8165f6bb3923c25c1f04ff607c
ntantai2526 <ntantai2526@gmail.com>
2023-07-20 12:12:01 +0700

    ManagerProduct - UI, scss, ts - update: tạo giao diện của manager-product, lấy api để hiển thị lên website

--- src/app/p-shop/pages/manager-product/manager-product.component.ts
+++ src/app/p-shop/pages/manager-product/manager-product.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { ShopApiService } from '../../share/services/shop-api.service';
+import { DTOProduct } from '../../share/dtos/DTOProduct';
 
 @Component({
   selector: 'app-manager-product',
   templateUrl: './manager-product.component.html',
-  styleUrls: ['./manager-product.component.scss']
+  styleUrls: ['./manager-product.component.scss'],
 })
-export class ManagerProductComponent {
+export class ManagerProductComponent implements OnInit {
+  arrUnsubscribe: Subscription[] = [];
 
+  constructor(private apiService: ShopApiService) {}
+
+  data: DTOProduct[];
+  listProduct: DTOProduct[];
+
+  ngOnInit(): void {
+    this.GetListProduct(1, 100, undefined);
+  }
+
+  //#region Our product
+
+  GetListProduct(page?: number, pageSize?: number, sort?: string) {
+    let GetListProduct = this.apiService
+      .getListProduct(page, pageSize, sort)
+      .subscribe(
+        (v: any) => {
+          this.listProduct = v.products;
+        },
+        (errr) => {
+          console.log(errr);
+        }
+      );
+
+    this.arrUnsubscribe.push(GetListProduct);
+  }
+
+  onclick() {
+    console.log('click thành công');
+  }
 }
