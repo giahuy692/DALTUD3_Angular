04a41569ac0d6a61c4969b3c254862caa12cdef2
giahuy692 <yuhaigod8485@gmail.com>
2023-07-22 11:26:24 +0700

    ManagerUser - UI - Create: xây layout cho trang quản lý user

--- src/app/p-shop/pages/manager-user/manager-user.component.ts
+++ src/app/p-shop/pages/manager-user/manager-user.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-manager-user',
   templateUrl: './manager-user.component.html',
-  styleUrls: ['./manager-user.component.scss']
+  styleUrls: ['./manager-user.component.scss'],
 })
 export class ManagerUserComponent {
+  handleSearch() {}
 
+  onKeyUp(event: any) {}
 }
